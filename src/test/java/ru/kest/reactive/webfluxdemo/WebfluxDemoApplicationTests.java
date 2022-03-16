faed5a9a13564be0dbdd785bea95d0bfd855430f
Konstantin Kharitonov <kkharitonov@it-one.ru>
2022-03-16 22:15:06 +0300

    init commit

---
+++ src/test/java/ru/kest/reactive/webfluxdemo/WebfluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kest.reactive.webfluxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
